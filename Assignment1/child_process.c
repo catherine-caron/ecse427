a10b5549c39fac42f4456e4a4a76c18bab49b4a2
catherine-caron <catherine.caron2@mail.mcgill.ca>
2021-10-09 19:14:19 +0000

    small progress

--- Assignment1/child_process.c
+++ Assignment1/child_process.c
@@ -83,7 +83,38 @@ int main(void) {
         printf("Please enter a command: ");     /*   display a prompt  */
         getcmd("\n>> ", args, &bg);     /* parse input and fill args */
 
-        // pid = fork();
+        /* try built in commands */
+        if (strcmp(args[0], "exit") == 0){  /* exit the shell */
+            exit(0); 
+        }
+        else if (strcmp(args[0], "cd") == 0){   /* change directory*/ 
+            if ( args[1] == NULL) { /* no args defaults to ls */
+                args[0] = "ls";
+                args[1] = NULL;
+                execvp(args[0], args);
+            }
+            else if (chdir(args[1]) < 0 ) { /* change directory */
+                printf("*** ERROR: cd failed\n");
+                exit(1);
+            }
+            
+        }
+        else if (strcmp(args[0], "pwd") == 0){  /* display current working directory */
+            char cwd[PATH_MAX];
+            if (getcwd(cwd, sizeof(cwd)) != NULL) {
+                printf("Current working dir: %s\n", cwd);
+            } else {
+                printf("*** ERROR: pwd failed\n");
+                exit(1);
+            }
+        }
+        else if (strcmp(args[0], "fg") == 0){
+
+
+        }
+        else if (strcmp(args[0], "jobs") == 0){
+        
+        }
 
         if ( (child_pid = fork()) < 0) {     /* fork a child process  */
             printf("*** ERROR: forking child process failed\n");
@@ -93,42 +124,9 @@ int main(void) {
             // kill(child_pid, SIGINT);
             
             if (execvp(args[0], args) < 0) {     /* execute the command  */
-                /* try built in commands */
-                if (strcmp(args[0], "exit") == 0){  /* exit the shell */
-                    exit(0); 
-                }
-                else if (strcmp(args[0], "cd") == 0){   /* change directory*/ 
-                    if ( args[1] == NULL) { /* no args defaults to ls */
-                        args[0] = "ls";
-                        args[1] = NULL;
-                        execvp(args[0], args);
-                    }
-                    else if (chdir(args[1]) < 0 ) { /* change directory */
-                        printf("*** ERROR: cd failed\n");
-                        exit(1);
-                    }
-                    
-                }
-                else if (strcmp(args[0], "pwd") == 0){  /* display current working directory */
-                    char cwd[PATH_MAX];
-                    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-                        printf("Current working dir: %s\n", cwd);
-                    } else {
-                        printf("*** ERROR: pwd failed\n");
-                        exit(1);
-                    }
-                }
-                else if (strcmp(args[0], "fg") == 0){
-
-
-                }
-                else if (strcmp(args[0], "jobs") == 0){
                 
-                }
-                else {
-                    printf("*** ERROR: exec failed\n");
-                    exit(1);
-                }
+                printf("*** ERROR: exec failed\n");
+                exit(1);
                 
             }
         }
