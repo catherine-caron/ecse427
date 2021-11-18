3ee36e5771a86bd509e7e25e9205ad8cafe610dd
catherine-caron <catherine.caron2@mail.mcgill.ca>
2021-11-18 15:05:43 +0000

    initial structs

--- Assignment3/sfs_api.c
+++ Assignment3/sfs_api.c
@@ -14,8 +14,23 @@ void mksfs(int fresh) {
     // set i-node for file
     // R/W pointer to end of file
     // use on disk to set up memory
-
+/**
+* @brief 
+* 1. Allocate and initialize an i-Node. You need to somehow remember the state of the i-Node table to know which i-Node could be allocated for the newly created file. Simply remembering the last i- Node used is not correct because as you delete files, some i-Nodes in the middle of the table will become unused and available for reuse.
+* 2. Write the mapping between the i-Node and file name in the root directory. You could simply update the memory and disk copies.
+* 3. No disk data block allocated. File size is set to 0.
+* 4. This can also “open” the file for transactions (read and write). Note that the SFS API does not have a separate create() call. So you can do this activity as part of the open() call
+ */
+    
 }
+
+
+int sfs_fopen(char *name){
+     // opens the given file
+    // return index
+    // use on disk to set up memory
+}
+
 
 int sfs_getnextfilename(char *fname) {
     // get the name of the next file in directory 
@@ -28,21 +43,18 @@ int sfs_getfilesize(const char* path) {
     // from memory probs
 } 
 
-int sfs_fopen(char *name){
-     // opens the given file
-    // return index
-    // use on disk to set up memory
-}
-
-int sfs_fclose(int fileID) {
-    // closes the given file
-} 
-
 
 int sfs_fwrite(int fileID, char *buf, int length) {
     // write buf characters into disk
     // inc RW pointer
     // from mem
+/*
+1. Allocate disk blocks (mark them as allocated in your free block list).
+2. Modify the file's i-Node to point to these blocks.
+3. Write the data the user gives to these blocks.
+4. Flush all modifications to disk.
+5. Note that all writes to disk are at block sizes. If you are writing few bytes into a file, this might actually end up writing a block to next. So if you are writing to an existing file, it is important you read the last block and set the write pointer to the end of file. The bytes you want to write goes to the end of the previous bytes that are already part of the file. After you have written the bytes, you flush the block to the disk.
+*/
 } 
 
 int sfs_fread(int fileID, char *buf, int length) {
@@ -55,7 +67,13 @@ int sfs_fseek(int fileID, int loc) {
     // seek to the location from beginning
     // move RW pointer
     // from mem
+    // 1. Modify the read and write pointers in memory. There is nothing to be done on disk!
 } 
+
+int sfs_fclose(int fileID) {
+    // closes the given file
+} 
+
 
 int sfs_remove(char *file) {
     // removes a file from the filesystem
--- Assignment3/sfs_api.h
+++ Assignment3/sfs_api.h
@@ -1,8 +1,83 @@
 #ifndef SFS_API_H
 #define SFS_API_H
 
-// You can add more into this file.
+// size of components
+#define blockSize 1024          /* recommended size                                     */
+#define sizeOfPointer 4         /* bytes                                                */
+#define numberOfBlocks 1024     /*                                                      */
+#define numberOfInodes 200      /*  max number of iNodes is equal to number of blocks of the partition */
+#define sizeOfInode 64          /*                                                      */
+#define sizeOfSuperBlockField 4 /*                                                      */
+#define numberOfEntries 64      /*                                                      */
 
+/* structs */
+
+/* I-Node */
+// non standard inode 
+// size field  total number of bytes 
+// no need to know about indirect 
+// these block contains data
+
+//File has lots of inodes 
+// for j node   size = size of the file 
+// set size = -1 to be blank
+
+/* I-Node */
+typedef struct {
+	int size; // total number of bytes
+	int direct[14]; // 
+	int indirect;
+} inode_t;
+
+/* I-Node Block */
+typedef struct {
+	inode_t inodeSlot[16];
+} inodeBlock_t;
+
+// root is a jnode????
+// have to add shadow jnode later????
+// need to fill to get to 1024 or  copy memory to block_t then pass that to the disk * calloc
+
+/* Superblock */
+typedef struct {
+    unsigned char magic[4];             // magic number
+    int block_size;                     // equals 1024
+    int fs_size;                        // number of blocks
+    int Inodes;                         // iNode table length
+    inode_t root;                       // iNode number of root directory 
+    int rootDirectoryBlockNumber[4];    // 
+    
+    inode_t shadow[4];      // ?????
+    int lastShadow;
+
+    char fill[668];                     // unused space filled with empty values (may need to increase this amount)
+} superblock_t;
+
+/* Block */
+typedef struct{
+	unsigned char bytes[blockSize];     // 1024
+}block_t;
+
+/* File Descriptor */
+typedef struct {
+    int free;           // free space
+    int inode;          //
+    int rwptr;          // read/write pointer
+	// int readptr;     // probably not needed
+} fileDescriptor_t;
+ 
+/* Directory entry */
+typedef struct {
+    char name[10];      // limited name size
+    int inodeIndex;     // index in inode table
+} directoryEntry_t;
+
+/* Root Directory */
+typedef struct{
+	directoryEntry_t entries[64];   // special type of directory entry referencing all others
+} rootDirectory_t;
+
+/* methods */
 void mksfs(int);
 
 int sfs_getnextfilename(char*);
@@ -13,7 +88,7 @@ int sfs_fopen(char*);
 
 int sfs_fclose(int);
 
-int sfs_fwrite(int, const char*, int);
+int sfs_fwrite(int, char*, int);
 
 int sfs_fread(int, char*, int);
 
